4824fba5c7584bc0fa684f79a324b122b467a9bf
Kate Hoeffler <kate.hoeffler@team.telstra.com>
2025-11-18 20:03:42 +1100

    Adding test python to repositary

---
+++ test.py
@@ -0,0 +1,10 @@
+name = "Kobi"
+
+print(f"Hello {name}! It's {2*2} x cool to meet you!")
+
+my_favourite_number = 3
+
+times_five = my_favourite_number * 5
+
+print("I multiplied your favourite number by 5 and got...")
+print(times_five)
